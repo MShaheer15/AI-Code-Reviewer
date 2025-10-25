1f7aca73c7ae1a7f0bec9057fa91110d3e9eb9d7
MShaheer15 <shaheersiddiqui803@gmail.com>
2025-10-25 21:10:10 +0500

    Added vercel.json, api folder, and updated backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
 const app = require("./src/app");
 
-app.listen(process.env.PORT || 3000,function() {
-    console.log("Server is running on port 3000");
-})
+module.exports = app;
